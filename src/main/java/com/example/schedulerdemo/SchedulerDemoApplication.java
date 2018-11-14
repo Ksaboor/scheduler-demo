000475a37e83cbd9fc3f36d35b449a5a467bfd23
Khail Saboor <khalilsaboor1@gmail.com>
2018-11-14 16:29:02 -0500

    first commit

---
+++ src/main/java/com/example/schedulerdemo/SchedulerDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.schedulerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulerDemoApplication.class, args);
+	}
+}
